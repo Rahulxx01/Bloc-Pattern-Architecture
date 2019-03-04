7da1c88b9a1a6620cecd77bcd3fdf40fcff7c1b0
Rahul Yadav <rahulyadav@Rupals-MacBook-Pro.local>
2019-03-05 00:22:54 +0530

    First Commit
    First Commit

---
+++ bloc_pattern_architecture/lib/main.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+import 'src/ui/app.dart';
+
+
+void main(){
+  runApp(App());
+}
+
+
+
---
+++ bloc_pattern_architecture/lib/src/resources/constants.dart
@@ -0,0 +1,2 @@
+
+final String MOVIES_URL_POPUlAR = "https://api.themoviedb.org/3/movie/popular?api_key=403aab985eeaff498624b420c392e97c";
